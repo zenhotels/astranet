001c16728746f8531330ee0bab71cb08ce7e4a11
Ilya Biin <ibiin@ostrovok.ru>
2016-09-22 10:51:01 +0300

    limit adjusted dor travis-ci

--- tests.sh
+++ tests.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 go test -cpu 2 -v -race $(find . -type d -not -path '*vendor*' -not -path '*.git*' )
 go run -race example/loopback/main.go
-go run -race example/hurtmeplenty/main.go -streams=4096
+go run -race example/hurtmeplenty/main.go -streams=2048
